b0ba980c56cd543c1e557f75432fe15f19d09544
Simran Sharma <simransharma33166@gmail.com>
2023-10-24 23:31:00 +0530

    first commit

---
+++ foodDistributionManagement/foodDistributionManagement/src/main/java/com/simran/demo/foodDistributionManagement/FoodDistributionManagementApplication.java
@@ -0,0 +1,20 @@
+package com.simran.demo.foodDistributionManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.simran.demo.controller","com.simran.demo.model","com.simran.demo.repository"})
+@EnableJdbcRepositories(basePackages = {"com.simran.demo.repository"})
+@EntityScan(basePackages = {"com.simran.demo.model"})
+public class FoodDistributionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDistributionManagementApplication.class, args);
+	}
+
+}
---
+++ foodDistributionManagement/foodDistributionManagement/src/test/java/com/simran/demo/FoodDistributionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simran.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDistributionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
